47df8de09e4e05baf2f66df05d0a151eb6f4319a
kenpeter <kenpeter>
2017-10-31 16:43:47 +1100

    not working

--- client/index.js
+++ client/index.js
@@ -1,10 +1,24 @@
 // vue
 import Vue from 'vue'
+//
+import VueRouter from 'vue-router'
 // app
 import App from './components/App.vue'
+
+// Use router
+Vue.use(VueRouter)
+
+const routes = [
+	{path: '/', component: App}
+]
+
+const router = new VueRouter({
+	routes
+})
 
 // #app
 new Vue({
   el: '#app',
+	router,
   render: h => h(App)
 })
